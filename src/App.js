14ffcf1f17d690795752ac6b55fd41d3bae532cc
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-06-12 11:45:02 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./logo.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <img src={logo} className="App-logo img-fluid" alt="logo" />
+      Hello
+    </div>
+  );
+}
