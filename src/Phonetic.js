d3ab2bb51b45a12b78a30975521afd0ccd36d395
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-06-14 19:52:20 -0400

    added noreferrer

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -5,7 +5,7 @@ export default function Phonetic(props) {
   if (props.phonetic)
     return (
       <div className="Phonetic">
-        <a href={props.phonetics.audio} target="_blank">
+        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
           Listen
         </a>
         <span className="text">{props.phonetics.text}</span>
