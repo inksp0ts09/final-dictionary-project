8e9c9b90bb30b2b10786ffd773aa8e51ba4d97f4
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-06-16 07:41:21 -0400

    updated Phonetics
    took out if statement

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -2,13 +2,12 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  if (props.phonetic)
-    return (
-      <div className="Phonetic">
-        <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-          Listen
-        </a>
-        <span className="text">{props.phonetics.text}</span>
-      </div>
-    );
+  return (
+    <div className="Phonetic">
+      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
+        Listen
+      </a>
+      <span className="text">{props.phonetics.text}</span>
+    </div>
+  );
 }
