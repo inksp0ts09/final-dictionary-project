26294648a7f47b8644495ba5d039a0899b620a67
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-07-02 23:54:43 -0400

    new logo, new colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
+import logoGit from "./logoGit.png";
 import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header"></header>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
         <main>
-          <Dictionary defaultKeyword="sunset" />
+          <Dictionary defaultKeyword="forrest" />
         </main>
         <footer className="App-footer">
           <small>Coded by Maryanne McGlone</small>
