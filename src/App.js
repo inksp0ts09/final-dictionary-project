f3d284fe79ac31aa5226aaa5fa3a26b299b8542b
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2022-07-03 19:11:46 -0400

    Revert "changed container className to element"
    This reverts commit 8d278e705c60d46f3a2c0959345633f198b8dd57.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Dictionary from "./Dictionary";
 export default function App() {
   return (
     <div className="App">
-      <container>
+      <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
@@ -15,7 +15,7 @@ export default function App() {
         <footer className="App-footer">
           <small>Coded by Maryanne McGlone</small>
         </footer>
-      </container>
+      </div>
     </div>
   );
 }
--- src/Photos.js
+++ src/Photos.js
@@ -5,10 +5,10 @@ export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        <row>
+        <div className="row">
           {props.photos.map(function (photo, index) {
             return (
-              <col md={4} key={index}>
+              <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
@@ -16,10 +16,10 @@ export default function Photos(props) {
                     alt={photo.alt}
                   />
                 </a>
-              </col>
+              </div>
             );
           })}
-        </row>
+        </div>
       </section>
     );
   } else {
